ae723ad808a4d2dd168c2652332dae98dac0ae6a
Артём Мельчаков <maw@pbprog.ru>
2023-07-12 23:37:50 +0300

    Add project files.

---
+++ DupUtilityMauiApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DupUtilityMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DupUtilityMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DupUtilityMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
